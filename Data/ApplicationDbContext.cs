5599cc8faebcbb6729bb76153b65fc5f8d3510df
DaltonOsborne <rainbowrocko24@hotmail.com>
2022-07-15 18:55:14 -0400

    heres more stuff

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,5 +9,23 @@ namespace PM_MovieRandomizer.Data
             : base(options)
         {
         }
+
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            {
+                Id = Guid.Parse("9792CB5B - 09A3 - 4B49 - 94A1 - 036D494B1BBE"),
+                Title = "Pokemon: The First Movie",
+                ReleaseDate = DateTime.Parse("06-08-1998"),
+                OpeningWeekendSales = 345345345,
+                TotalSales = 34534534,
+                Rating = Rating.E
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿namespace PM_MovieRandomizer.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Rating Rating { get; set; }
+        public double OpeningWeekendSales { get; set; }
+
+        public double TotalSales { get; set; }
+    }
+    public enum Rating
+    {
+        E
+    }
+}
