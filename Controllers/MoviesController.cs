41c7d2f5d73977da55f63a7550e38728eb672a3b
DaltonOsborne <rainbowrocko24@hotmail.com>
2022-07-15 20:06:50 -0400

    added movie tab for website

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using PM_MovieRandomizer.Models;
 
 namespace PM_MovieRandomizer.Controllers
 {
+    [Authorize]
     public class MoviesController : Controller
     {
         private readonly ApplicationDbContext _context;
